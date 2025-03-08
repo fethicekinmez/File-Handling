954c515ec807bc4d70ad928653515abbccd6bd71
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-03-09 00:10:32 +0300

    Add files via upload

--- Write/main.cpp
+++ Write/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int main(){
+
+    fstream myFile;
+    myFile.open("file.txt", ios::out);
+    if(myFile.is_open()){
+        cout << "writing into file..." << endl;
+        myFile << "Hello" << endl;
+        myFile << "Second Line\n";
+        myFile << "Third Line";
+    }
+    myFile.close();
+
+    return 0;
+}
