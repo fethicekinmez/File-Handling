b28410fd520bc0812a92cbaa362ca202d4a59314
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-03-09 00:12:06 +0300

    Add files via upload

--- ifstream_ofstream/main.cpp
+++ ifstream_ofstream/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+int main(){
+    string s1 = "Lemon and lemonade";
+
+    ofstream file("file.txt");
+    file << s1;
+    file.close();
+
+    ifstream file2("file.txt");
+    string s2;
+    while (file2 >> s2)
+    {
+        cout << s2 << endl;
+    }
+    
+    file2.close();
+
+    return 0;
+}
