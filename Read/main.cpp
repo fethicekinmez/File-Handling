641ced28a70683271b91790d0f6c5c71ac508a7e
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-03-09 00:08:34 +0300

    Add files via upload

--- Read/main.cpp
+++ Read/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+int main(){
+
+    fstream myFile;
+    myFile.open("text.txt", ios::in);
+
+    if(myFile.is_open()){
+        string line;
+        while(getline(myFile, line)){
+            cout << line << endl;
+        }
+        myFile.close();
+    }
+
+    return 0;
+}
