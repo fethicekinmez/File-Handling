f22dfff8c59fb0d1142a385054f28fecdf08490a
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-03-09 00:11:09 +0300

    Add files via upload

--- Append/main.cpp
+++ Append/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+    fstream myFile;
+    myFile.open("text.txt", ios::out);
+    if(myFile.is_open()){
+        myFile << "First Line" << endl;
+        myFile << "Second Line" << endl;
+        myFile << "Third Line" << endl;
+    }
+    myFile.close();
+
+    myFile.open("text.txt", ios::app);
+    if(myFile.is_open()){
+        myFile << "Appended Line" << endl;
+    }
+    myFile.close();
+    
+
+    return 0;
+}
 
